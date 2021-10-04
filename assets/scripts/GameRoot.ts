853f44310ddbad81356c23e4deea700e8f9f4849
yangkuo <2721791961@qq.com>
2021-10-04 20:45:13 +0800

    feat: 寻找钥匙功能

--- assets/scripts/GameRoot.ts
+++ assets/scripts/GameRoot.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node, tween } from 'cc'
+import { _decorator, Component, Node, tween, v3 } from 'cc'
 import { MapData, MapRoot } from './MapRoot'
 import { AStarObj, Util } from './Util'
 const { ccclass, property } = _decorator
@@ -25,6 +25,9 @@ export class GameRoot extends Component {
                 this.goAstarPath(result, 0)
             } else {
                 // 修改的 a 星，去寻找钥匙，对应 two 场景
+                Util.aStarPathFindDoorKey(mapData)
+                // 找钥匙是另一个搜索，做动画仅为示意用
+                this.demoDoorKey()
             }
         }, 1)
     }
@@ -38,6 +41,27 @@ export class GameRoot extends Component {
                 index++
                 this.goAstarPath(arr, index)
             })
+            .start()
+    }
+
+    demoDoorKey() {
+        // a 星搜索到门的动画
+        tween(this.boy)
+            .to(2.0, { position: v3(150, 550, 0) })
+            .to(0.5, { position: v3(250, 550, 0) })
+            .to(0.5, { position: v3(250, 450, 0) })
+            .to(0.5, { position: v3(250, 550, 0) })
+            .to(1.5, { position: v3(550, 550, 0) })
+            .to(1.0, { position: v3(550, 750, 0) })
+            .to(0.5, { position: v3(650, 750, 0) })
+            .to(0.5, { position: v3(650, 850, 0) })
+            // 递归寻找的，嗖的一下就过去
+            .to(0.2, { position: v3(150, 550, 0) })
+            .to(2.0, { position: v3(150, 950, 0) })
+            // 开门
+            .to(0.2, { position: v3(250, 450, 0) })
+            .to(1.5, { position: v3(250, 150, 0) })
+            .to(3.5, { position: v3(950, 150, 0) })
             .start()
     }
 }
